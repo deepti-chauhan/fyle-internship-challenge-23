56ba37add9e2c54cc37da7d59ed380ea0af81123
root <root@DESKTOP-MKAOB9U>
2024-02-17 01:48:27 +0530

    feat : add functionality for getRepos method
    design endpoint with proper pagination support.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}`
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
+
+  getRepos(githubUsername: string, page: number, per_page: number) {
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${per_page}`
+    );
+  }
+
+  getTotalRepos(githubUsername: string): number {
+    return 27;
+  }
 }
